b4ded9d955bcc3d02bf2f62964142ed02ce1e05c
matiasq <mquispe1185@gmail.com>
2022-08-26 19:09:53 -0300

    Setup omniauth-google

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, Rails.application.credentials.dig(:google_id), Rails.application.credentials.dig(:google_key), {}
+  #config.omniauth :google_oauth2, Rails.application.credentials.dig(:google_id), Rails.application.credentials.dig(:google_key), {}
+  #config.omniauth.allowed_request_methods = [:post, :get]
   # ==> Warden configurationgi
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
