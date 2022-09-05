7c60ff7886b7507085f157443138f026f1ff99c8
eduardoaz5 <edu0596@gmail.com>
2022-09-05 19:48:33 -0300

    fix seeds for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,53 +3,53 @@
 
 if User.count.zero?
   3.times do |i|
-    User.create(email: "user#{i}@example.com",
+    User.create(email: "user#{i+1}@example.com",
                 password: "user123",
-                name: "User #{i}",
+                name: "User #{i+1}",
                 image: "https://lh3.googleusercontent.com/example")
   end
 end
 if Province.count.zero?
   3.times do |i|
-    Province.create(name: "Province #{i}")
+    Province.create(name: "Province #{i+1}")
   end
 end
 if City.count.zero?
   3.times do |i|
-    City.create(name: "City #{i}", 
-                province_id: i)
+    City.create(name: "City #{i+1}", 
+                province_id: i+1)
   end
 end
 if Category.count.zero?
   3.times do |i|
-    Category.create(name: "Category #{i}")
+    Category.create(name: "Category #{i+1}")
   end
 end
 if Ong.count.zero?
-  4.times do |i|
-    Ong.create(name: "Ong #{i}",
+  3.times do |i|
+    Ong.create(name: "Ong #{i+1}",
                description: "This Ong description.",
-               city_id: i,
-               province_id: i,
+               city_id: i+1,
+               province_id: i+1,
                phone: 1111-2222-3333,
-               email: "ong#{i}@example.com",
-               user_id: i,
-               category_id: i,
-               status_updated_by_id: i,
+               email: "ong#{i+1}@example.com",
+               user_id: i+1,
+               category_id: i+1,
+               status_updated_by_id: i+1,
                video_url: "http://example.com/")
   end
 end
 if Campaign.count.zero?
-  4.times do |i|
-    Campaign.create(name: "Campaign #{i}",
+  3.times do |i|
+    Campaign.create(name: "Campaign #{i+1}",
                description: "This Campaign description.",
-               city_id: i,
-               province_id: i,
+               city_id: i+1,
+               province_id: i+1,
                phone: 1111-2222-3333,
-               email: "ong#{i}@example.com",
-               user_id: i,
-               category_id: i,
-               status_updated_by_id: i,
+               email: "ong#{i+1}@example.com",
+               user_id: i+1,
+               category_id: i+1,
+               status_updated_by_id: i+1,
                video_url: "http://example.com/")
   end
 end
