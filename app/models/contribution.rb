a9699a5907a1f49add6f6594c5f81a38f99f29b5
josegabriels <jose.gabriels12@gmail.com>
2023-01-18 21:12:20 -0300

    some changes made in the contribution files

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -2,4 +2,12 @@ class Contribution < ApplicationRecord
   belongs_to :item_donation
   belongs_to :user
   belongs_to :status_updated_by, class_name: 'User'
+
+  enum status: [:pendiente, :entregado, :no_entregado]
+
+  before_create :gen_code
+
+  def gen_code
+    code = SecureRandom.urlsafe_base64(6)
+  end
 end
