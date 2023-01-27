898642567a57a952560c46bbf68f0c3ebe31903e
eduardoaz5 <edu0596@gmail.com>
2023-01-27 04:59:44 -0300

    add fixedcost and user params in preference_data and add encrypted mercadopago production credentials for test user vendedor

--- app/controllers/mercadopago_controller.rb
+++ app/controllers/mercadopago_controller.rb
@@ -1,16 +1,23 @@
 class MercadopagoController < ApplicationController
   def checkout
     require 'mercadopago'
-    sdk = Mercadopago::SDK.new('ACCESS_TOKEN')
+    sdk = Mercadopago::SDK.new(Rails.application.credentials.dig(:mp_acctoken_prod))
+    fixed_cost = FixedCost.find(params[:fixed_cost_id])
     preference_data = {
       items: [
         {
-          title: 'Mi producto',
-          unit_price: 75.56,
+          title: fixed_cost.title,
+          unit_price: params[:amount].to_f,
           quantity: 1,
-          id: params[:fixed_cost_id],
-        }
+          id: fixed_cost.id,
+          description: fixed_cost.description,
+        },
       ],
+      payer: {
+        name: current_user.first_name,
+        surname: current_user.last_name,
+        email: current_user.email,
+      },
       back_urls: {
         success: 'http://localhost:4200/ong',
         pending: 'http://localhost:4200/ong',
@@ -19,7 +26,7 @@ class MercadopagoController < ApplicationController
     }
     preference_response = sdk.preference.create(preference_data)
     preference = preference_response[:response]
-    
+
     @preference_id = preference['id']
     render json: {preference_id: @preference_id}
   end
